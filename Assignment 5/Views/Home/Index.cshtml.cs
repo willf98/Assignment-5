5793fe9df9b00be80dfb7f61db6543b019fed1b9
willf98 <willf98@byu.edu>
2026-02-06 18:27:36 -0700

    Add project files.

---
+++ Assignment 5/Views/Home/Index.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Assignment_5.Views.Home
+{
+    public class IndexModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
